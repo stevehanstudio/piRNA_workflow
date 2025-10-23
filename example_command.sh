a77c9a25a6ccaad067abaa98de31a4ef29c2d2e4
Steve Han <15229911+stevehanstudio@users.noreply.github.com>
2025-10-23 16:49:49 -0700

    Update example_command.sh to run on server

--- example_command.sh
+++ example_command.sh
@@ -4,10 +4,10 @@
 # This allows testing the workflow without duplicating large reference files
 
 ./run_workflow.sh 1 run \
-  --genome-path ~/Projects/HeLab/piRNA_workflow/Shared/DataFiles/genome/dm6.fa \
-  --index-path ~/Projects/HeLab/piRNA_workflow/Shared/DataFiles/genome/bowtie-indexes/dm6 \
-  --dataset-path ~/Projects/HeLab/piRNA_workflow/Shared/DataFiles/datasets/chip-seq/chip_inputs \
-  --vector-path ~/Projects/HeLab/piRNA_workflow/Shared/DataFiles/genome/YichengVectors/42AB_UBIG \
-  --adapter-path ~/Projects/HeLab/piRNA_workflow/Shared/DataFiles/genome/AllAdaptors.fa \
+  --genome-path /mnt/dev0/hans/piRNA_workflow/Shared/DataFiles/genome/dm6.fa \
+  --index-path /mnt/dev0/hans/piRNA_workflow/Shared/DataFiles/genome/bowtie-indexes/dm6 \
+  --dataset-path /mnt/dev0/hans/piRNA_workflow/Shared/DataFiles/datasets/chip-seq/chip_inputs \
+  --vector-path /mnt/dev0/hans/piRNA_workflow/Shared/DataFiles/genome/YichengVectors/42AB_UBIG \
+  --adapter-path /mnt/dev0/hans/piRNA_workflow/Shared/DataFiles/genome/AllAdaptors.fa \
   --cores 12
 
